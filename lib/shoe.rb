58a73fc4e52383125542493848522debac76d641
Taylor Swinton <taylorswinton1@gmail.com>
2019-02-19 19:35:32 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -5,14 +5,10 @@ class Shoe
 
   attr_reader :brand
 
-  attr_accessor :color, :size, :material, :condition
+  attr_accessor :color, :size, :material,
 
   def cobble
     puts "Your shoe is as good as new!"
-  end
-
-  def condition
-    @condition = "new"
   end
 
 end
